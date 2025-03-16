c2f5a93e7cf402a7c617b05b6a3ea14e0120c27a
Steffen-Svenningsen <stef7625@gmail.com>
2025-03-16 13:54:28 +0100

    Make ready for prod

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -30,7 +30,7 @@ export default function TextEditor() {
     const [quill, setQuill] = useState()
 
     useEffect(() => {
-        const s = io("http://localhost:3001")
+        const s = io(import.meta.env.VITE_API_URL || 'http://localhost:3001')
         setSocket(s)
 
         return () => {
