cce58cefb3235d5d2692c0b297e4699f6fd4afd8
Steffen-Svenningsen <stef7625@gmail.com>
2025-05-05 09:03:23 +0200

    Refactor aside

--- src/components/AsideToolbar.jsx
+++ src/components/AsideToolbar.jsx
@@ -1,12 +1,17 @@
 import "../styles/aside.css"
 import { Button } from "@chakra-ui/react"
 import { Toaster, toaster } from "./ui/toaster"
-import { ZoomIn, ZoomOut, Pen, Type } from "lucide-react"
+import { ZoomIn, ZoomOut, Pen, Type, Eye, EyeOff } from "lucide-react"
 import { useState } from "react"
 
 
 export default function AsideToolbar() {
     const [zoomLevel, setZoomLevel] = useState(100)
+    const [mainControls, setMainControls] = useState(true)
+
+    const handleControls = () => {
+        setMainControls(prev => !prev)
+    }
     
     const zoomIn = () => {
         if (zoomLevel < 150) {
@@ -60,20 +65,27 @@ export default function AsideToolbar() {
     return (
         <>
             <aside>
-                <div className="zoom-controls">
-                    <Button variant={'ghost'} className="zoom-out" onClick={zoomOut} aria-label="Zoom out">
-                        <ZoomOut size={14} />
-                    </Button>
-                    <Button variant={'ghost'} className="zoom-in" onClick={zoomIn} aria-label="Zoom in">
-                        <ZoomIn size={14} />
-                    </Button>
+                {mainControls && (
+                    <div className="aside-item-wrapper main-controls">
+                        <div className="zoom-controls">
+                            <Button variant={'ghost'} className="zoom-out" onClick={zoomOut} aria-label="Zoom out">
+                                <ZoomOut size={14} />
+                            </Button>
+                            <Button variant={'ghost'} className="zoom-in" onClick={zoomIn} aria-label="Zoom in">
+                                <ZoomIn size={14} />
+                            </Button>
+                        </div>
+                        <Button variant={'ghost'}>
+                            <Pen size={14} />
+                        </Button>
+                        <Button variant={'ghost'}>
+                            <Type size={14} />
+                        </Button>
+                    </div>
+                )}
+                <div className="aside-item-wrapper">
+                    <Button onClick={handleControls} aria-label="Toggle controls" variant={'ghost'} className="menu-trigger">{mainControls ? <Eye size={14} /> : <EyeOff size={14}/>}</Button>
                 </div>
-                <Button variant={'ghost'}>
-                    <Pen size={14} />
-                </Button>
-                <Button variant={'ghost'}>
-                    <Type size={14} />
-                </Button>
             </aside>
             <Toaster />
         </>
